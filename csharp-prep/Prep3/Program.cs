31c75875c731dc62ceca6c18248291021db751b6
Tsung Ying Wu <home@Macmini.localdomain>
2024-09-26 23:16:54 +0800

    Guess Number

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,61 @@
-using System;
+using System; 
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // declare variable
+        int magic_number,guess_number;
+        string message;
+        bool isContinue = true;
+        int count = 0;
+        string Answer = "Yes";
+        bool isPlayAgain = true;
+        
+        while (isPlayAgain == true) {
+            Console.Write("vWhat is the magic number ? ");
+            magic_number = int.Parse(Console.ReadLine());
+            while (isContinue == true) {
+
+                Console.Write("vWhat is your guess ? ");
+                guess_number = int.Parse(Console.ReadLine());
+                if (guess_number == magic_number) {
+                    message = "You guessed it";
+                    isContinue = false;
+                } 
+                else if (guess_number > magic_number) {
+                    message = "Lower";
+                }
+                else if (guess_number < magic_number) {
+                    message = "Heigher";
+                }
+                else {
+                    message = "none";
+                }  
+                // display message
+                Console.WriteLine(message);
+                count = count +1;
+                //Console.WriteLine();  
+            
+            }
+            Console.WriteLine($"You have guessed {count} times");
+            Console.WriteLine($"Do you want to play the game again ? (Yes/No) ");
+            Answer = Console.ReadLine();
+            
+            if (Answer.ToUpper() == "YES") {
+                isPlayAgain = true;
+                isContinue = true;
+                
+            }
+            else {
+                isPlayAgain = false;
+            } 
+            count = 0;
+            
+        }   
     }
 }
+
+
+
+
